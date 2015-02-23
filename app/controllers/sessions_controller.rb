1892fea29e2109d9db84cbce543fe2dee23ff407
Danielle <danielle@daniellemullane.com>
2015-02-23 10:53:28 -0700

    Complete sessions controller, add has_secure_password to application controller, define new in users controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,23 @@
 class SessionsController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.find_by(user_name: params[:user_name])
+    if @user and @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: "You have successfully logged in."
+    else
+      flash[:error] = "Username / password combination is invalid."
+      render :new
+    end
   end
 
   def destroy
     session.clear
-    redirect to root_path
+    redirect to root_path, notice: "You have successfully logged out."
   end
 
 end
