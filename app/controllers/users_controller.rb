b211f519753980314c97279a7ec05ae2b1825afc
Danielle <danielle@daniellemullane.com>
2015-02-23 11:55:21 -0700

    Add functional signup page for users#new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,20 @@ class UsersController < ApplicationController
   end
 
   def new
+    puts "HELLO"*5
     @user = User.new
+    puts @user
   end
 
   def create
+    @user = User.new(user_params)
 
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
     get '/login' => 'sessions#new'
     post '/login' => 'sessions#create'
     get '/logout' => 'sessions#destroy'
+    get '/signup' => 'users#new'
+    post '/signup' => 'users#create'
 
 end
