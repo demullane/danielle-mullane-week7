5c8fa9deef29c26bc2e1f9c940b00fc7f4a670cf
Danielle <danielle@daniellemullane.com>
2015-02-23 11:36:09 -0700

    Complete login and logout actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,9 @@ class SessionsController < ApplicationController
   end
 
   def create
+
     @user = User.find_by(user_name: params[:user_name])
+
     if @user and @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect_to root_path, notice: "You have successfully logged in."
@@ -13,11 +15,12 @@ class SessionsController < ApplicationController
       flash[:error] = "Username / password combination is invalid."
       render :new
     end
+
   end
 
   def destroy
     session.clear
-    redirect to root_path, notice: "You have successfully logged out."
+    redirect_to root_path, notice: "You have successfully logged out."
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,39 @@
 class UsersController < ApplicationController
 
+  before_action :find_params, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
 
+  def create
+
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def find_params
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:user_name, :password, :about)
+  end
 
 end
